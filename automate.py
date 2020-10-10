69cf6b7cc5c93712363aafc0c217eeb829082be8
c0braKai <51086253+MK-Kaiser@users.noreply.github.com>
2020-10-10 09:18:43 -0500

    Update automate.py

--- automate.py
+++ automate.py
@@ -3,9 +3,11 @@
 # Author: Mark Kaiser
 # Date: 8/9/20
 # Description: automates a command when given a list of names, users, processes, services or filenames
-import subprocess
+from subprocess import Popen, PIPE
 
-command = "echo"
-with open('items.txt', 'r', encoding='latin') as lst:
+command = 'echo'
+fileName = 'items.txt'
+with open(fileName, 'r', encoding='latin') as lst:
     for i in lst:
-        print(subprocess.Popen(command + " " + i, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.read().strip())
+        result = Popen(command + " " + i, shell=True, stdout=PIPE, stderr=PIPE)
+        print(result.stdout.read().strip())
